5c48af521b0712f159d30472557cd8bbb34a667b
Mim Collopy <mim.collopy@gmail.com>
2023-07-18 12:07:33 +1000

    update zoom feature

--- client/client.js
+++ client/client.js
@@ -1,26 +1,27 @@
-  
-  // import {fetchStations } from './servos_api.js'
+// import {fetchStations } from './servos_api.js'
 
-  // fetchStations().then(res => console.log(res))
-  
-  
-  let map
-  
-  async function initMap() {
-    // The location of Uluru
-    let position = { lat: -33.8717, lng: 151.2067 }
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary('maps')
-    const { AdvancedMarkerElement } = await google.maps.importLibrary('marker')
-  
-    // The map, centered at Uluru
-    map = new Map(document.getElementById('map'), {
-      zoom: 13,
-      center: position,
-      mapId: 'DEMO_MAP_ID',
-    })
-  
+// fetchStations().then(res => console.log(res))
+
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  let position = { lat: -33.8717, lng: 151.2067 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 13,
+    center: position,
+    minZoom: 9,
+    maxZoom: null,
+    
+    mapId: "DEMO_MAP_ID",
+  });
+
   //   for (let e of results) {
   //     let position = { lat: e.location.lat, lng: e.location.long }
   //     const marker = new AdvancedMarkerElement({
@@ -28,7 +29,6 @@
   //       map: map,
   //     })
   //   }
-  }
-  
-  initMap()
-  
+}
+
+initMap();
