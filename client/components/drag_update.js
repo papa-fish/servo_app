2a85da773198ea10cc2ab99d26730ebc3c9aefba
Mim Collopy <mim.collopy@gmail.com>
2023-07-19 15:22:32 +1000

    map centre 2/2 feature

--- client/components/drag_update.js
+++ client/components/drag_update.js
@@ -1,12 +1,12 @@
 // import { findAll } from "../../models/servo"
 
-import { fetchStations } from "../servos_api.js"
+// import { fetchStations } from "../servos_api.js"
 
-export function dragUpdate (map) {
-    map.addListener("dragend", () => {
-        console.log("dragging");
-        fetchStations
-    })
-}
+// export function dragUpdate (map) {
+//     map.addListener("dragend", () => {
+//         console.log("dragging");
+//         fetchStations
+//     })
+// }
 
-dragUpdate(map);
+// dragUpdate(map);
--- client/components/map_centre.js
+++ client/components/map_centre.js
@@ -1,21 +1,47 @@
+
 const mapCenter = document.querySelector(".center-location");
 
-export function mapCentreLocation(position, map) {
+// const geocoder = new google.maps.Geocoder();
+
+export async function mapCentreLocation(position, map) {
+  const { Geocoder } = await google.maps.importLibrary("geocoding"); 
+  const geocoder = new Geocoder()
+
+  geocoder
+    .geocode({ location: {lat: position.lat, lng: position.lng}} )
+    .then(response => {
+      console.log(response)
+      const address = response.results[0].formatted_address
     mapCenter.innerHTML =`
         <h3>MAP CENTRE LOCATION</h3>
         <p>Lat: ${position.lat} </p>
         <p>Long: ${position.lng} </p>
+        <p><strong>${address} </strong></p>
+        
     `
+})
     
   map.addListener("dragend", () => {
     let centerLocation = map.getCenter();
     let lat = centerLocation.lat();
     let long = centerLocation.lng();
- 
-    mapCenter.innerHTML =`
-        <h3>MAP CENTRE LOCATION</h3>
-        <p>Lat: ${lat} </p>
-        <p>Long: ${long} </p>
-      `
+
+    geocoder
+    .geocode({ location: {lat: lat, lng: long}} )
+    .then(response => {
+      console.log(response)
+      const address = response.results[0].formatted_address
+      
+          mapCenter.innerHTML =`
+              <h3>MAP CENTRE LOCATION</h3>
+              <p>Lat: ${lat} </p>
+              <p>Long: ${long} </p>
+              p><strong>${address} </strong></p>
+            
+            `
+    })
+    
+
+      
   })
-};
+}
